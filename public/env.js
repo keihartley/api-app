7ab5018de973113411501ec937dda3f28e2225ef
Kei Hartley <Clifford.kei.hartley@gmail.com>
2022-11-20 02:03:07 -0800

    Gitignore

---
+++ public/env.js
@@ -0,0 +1,9 @@
+window.env = {
+  "MEASUREMENT_ID": "G-EC42WSED04",
+  "APP_ID": "1:145846082095:web:f9ee3f595b36a41dea44da",
+  "MESSAGING_SENDER_ID": "145846082095",
+  "STORAGE_BUCKET": "api-app-23303.appspot.com",
+  "PROJECT_ID": "api-app-23303",
+  "AUTH_DOMAIN": "api-app-23303.firebaseapp.com",
+  "API_KEY": "AIzaSyDcbyD2V36E3u40d53F_y1xAibC9WTTS7Y"
+};
