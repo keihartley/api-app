c1c912f2480c99734d72d996ee812693666904a8
Kei Hartley <Clifford.kei.hartley@gmail.com>
2022-11-23 00:14:24 -0800

    Adding User Menu & search

--- src/Layout/Pages/Dashboard.jsx
+++ src/Layout/Pages/Dashboard.jsx
@@ -1,18 +1,91 @@
-import React, { useEffect } from "react";
-import { Box, AppBar, Toolbar, Typography, Button } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import {
+  Box,
+  AppBar,
+  Toolbar,
+  Typography,
+  InputBase,
+  Avatar,
+  Tooltip,
+  IconButton,
+  MenuItem,
+  Divider,
+  ListItemIcon,
+  Menu,
+} from "@mui/material";
+import { styled, alpha } from "@mui/material/styles";
 import { logout } from "../../Tools/Firebase/auth";
 import { useNavigate } from "react-router-dom";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../../Tools/Firebase/firebase";
+import {
+  AccountCircle,
+  Logout,
+  SearchRounded,
+  Settings,
+} from "@mui/icons-material";
 
 function Dashboard() {
   const navigate = useNavigate();
   const [user, loading] = useAuthState(auth);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     if (loading) return;
     if (!user) return navigate("/");
   }, [user, loading, navigate]);
+
+  const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: "inherit",
+    "& .MuiInputBase-input": {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+      transition: theme.transitions.create("width"),
+      width: "100%",
+      [theme.breakpoints.up("sm")]: {
+        width: "12ch",
+        "&:focus": {
+          width: "20ch",
+        },
+      },
+    },
+  }));
+
+  const Search = styled("div")(({ theme }) => ({
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    marginRight: theme.spacing(4),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(1),
+      width: "auto",
+    },
+  }));
+
+  const SearchIconWrapper = styled("div")(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  }));
+
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <div>
@@ -22,12 +95,91 @@ function Dashboard() {
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
               Dashboard
             </Typography>
-            <Button color="inherit" onClick={logout}>
-              Logout
-            </Button>
+            <Search>
+              <SearchIconWrapper>
+                <SearchRounded />
+              </SearchIconWrapper>
+              <StyledInputBase
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                placeholder="Search..."
+                inputProps={{ "aria-label": "search" }}
+              />
+            </Search>
+            <Tooltip title="Your Profile">
+              <IconButton
+                onClick={handleMenu}
+                size="small"
+                sx={{ ml: 2 }}
+                aria-controls={open ? "account-menu" : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? "true" : undefined}
+              >
+                <Avatar sx={{ width: 32, height: 32 }}>
+                  <AccountCircle />
+                </Avatar>
+              </IconButton>
+            </Tooltip>
           </Toolbar>
         </AppBar>
       </Box>
+      {user ? (
+        <Typography variant="h4">Welcome {user.displayName} </Typography>
+      ) : (
+        <></>
+      )}
+      <Menu
+        anchorEl={anchorEl}
+        id="account-menu"
+        open={open}
+        onClose={handleClose}
+        onClick={handleClose}
+        PaperProps={{
+          elevation: 0,
+          sx: {
+            overflow: "visible",
+            filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+            mt: 1.5,
+            "& .MuiAvatar-root": {
+              width: 32,
+              height: 32,
+              ml: -0.5,
+              mr: 1,
+            },
+            "&:before": {
+              content: '""',
+              display: "block",
+              position: "absolute",
+              top: 0,
+              right: 14,
+              width: 10,
+              height: 10,
+              bgcolor: "background.paper",
+              transform: "translateY(-50%) rotate(45deg)",
+              zIndex: 0,
+            },
+          },
+        }}
+        transformOrigin={{ horizontal: "right", vertical: "top" }}
+        anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+      >
+        <MenuItem>
+          <Avatar /> My account
+        </MenuItem>
+        <Divider />
+        <MenuItem>
+          <ListItemIcon>
+            <Settings fontSize="small" />
+          </ListItemIcon>
+          Settings
+        </MenuItem>
+        <MenuItem onClick={logout}>
+          <ListItemIcon>
+            <Logout fontSize="small" />
+          </ListItemIcon>
+          Logout
+        </MenuItem>
+      </Menu>
     </div>
   );
 }
